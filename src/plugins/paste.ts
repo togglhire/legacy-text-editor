cab6c191cea5a085a9d54df807eee34ea66cd322
Michal Miškerník <michal@miskernik.sk>
2018-08-30 18:42:56 +0100

    Pasting text as single text node

--- src/plugins/paste.ts
+++ src/plugins/paste.ts
@@ -1,17 +1,24 @@
 import { Plugin, getEventTransfer } from "slate-react";
-import { State } from "markup-it";
+import { State, BLOCKS } from "markup-it";
 import html from "markup-it/lib/html";
+import { Text, Block } from "slate";
 
 export const pastePlugin: Plugin = {
   onPaste: (event, change) => {
     const transfer = getEventTransfer(event);
-    if (transfer.type !== "html") return;
 
-    const source: string = (transfer as any).html;
+    if (transfer.type === "html") {
+      const source: string = (transfer as any).html;
 
-    const state = State.create(html);
-    const document = state.deserializeToDocument(source);
+      const state = State.create(html);
+      const document = state.deserializeToDocument(source);
 
-    return change.insertFragment(document);
+      return change.insertFragment(document);
+    }
+
+    if (transfer.type === "text") {
+      const source: string = (transfer as any).text;
+      return change.insertText(source);
+    }
   }
 };
