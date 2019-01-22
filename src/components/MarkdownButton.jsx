c0ba04d1d5a783e6f58dbd289314f430732d93b1
Michal Miškerník <michal@miskernik.sk>
2019-01-22 08:56:29 +0000

    Added titles to toolbar buttons

--- src/components/MarkdownButton.jsx
+++ src/components/MarkdownButton.jsx
@@ -2,9 +2,10 @@ import * as React from "react";
 import * as transforms from "../transforms";
 import { IconButton } from "./IconButton";
 
-export const MarkdownButton = ({ state, onChange, children }) => (
+export const MarkdownButton = ({ state, onChange, title, children }) => (
   <IconButton
     active={transforms.isInMarkdown(state)}
+    title={title}
     type="button"
     onClick={() => {
       onChange(transforms.toggleMarkdown(state));
